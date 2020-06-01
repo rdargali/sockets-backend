39f0a8729474a49b1b26ae07770f9001467e9aac
Rawand Dargali <rdargali@yahoo.com>
2020-05-31 23:48:41 -0500

    first

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-io.on("connection", () => {
-  /* … */
-});
-server.listen(3000);
